b969beff5997f4b6307d08cd66df74865b8fc4ad
zhuifeng1017@gmail.com <zhuifeng1017@gmail.com@ee969d1c-53b4-ea72-d881-057e2fa8ce14>
2012-12-14 11:01:02 +0000

    0000000
    git-svn-id: http://my-ios-study.googlecode.com/svn/trunk@3 ee969d1c-53b4-ea72-d881-057e2fa8ce14

---
+++ MyTestAll/MyTestAll2/MyTestAll2/MZPerson.h
@@ -0,0 +1,19 @@
+//
+//  MZPerson.h
+//  MyTestAll2
+//
+//  Created by uistrong on 12-12-14.
+//  Copyright (c) 2012年 uistrong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MZPerson : NSObject
+{
+    NSString *_name;
+}
+
+@property (retain, nonatomic) NSString *name;
+
+
+@end
